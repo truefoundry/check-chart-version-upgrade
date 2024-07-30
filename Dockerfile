b61ff5ab0304a3f14b8557b8263d3bfbf73bbedb
Dipo Ajayi <ajayidipo@ymail.com>
2024-07-30 23:50:42 +0100

    switched to javascript action

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.7-slim
-
-ADD . /app
-WORKDIR /app
-
-RUN pip install --upgrade pip requests
-
-CMD ["python", "check_chart_version_upgrade.py"]
---
+++ index.mjs
@@ -0,0 +1,105 @@
+import { Octokit } from "octokit";
+import path from "path";
+import core from "@actions/core";
+
+// Function to get the files changed in a pull request
+async function getFilesChanged(repo, pullRequestNumber) {
+    const [owner, repoName] = repo.split('/');
+    const octokit = new Octokit({
+        auth: process.env.GITHUB_TOKEN
+    });
+
+    let filesChangedData = [];
+    let page = 1;
+
+    while (true) {
+        const response = await octokit.rest.pulls.listFiles({
+            owner,
+            repo: repoName,
+            pull_number: pullRequestNumber,
+            per_page: 100,
+            page
+        });
+
+        const responseData = response.data;
+        if (responseData.length === 0) {
+            break;
+        }
+        filesChangedData = filesChangedData.concat(responseData);
+        page += 1;
+    }
+
+    return filesChangedData;
+}
+
+// Function to get the filenames from the files changed data
+function getFilenames(filesChangedData) {
+    return filesChangedData.map(file => file.filename);
+}
+
+// Function to get the charts directories changed
+function getChartsDirsChanged(filesChanged) {
+    const dirs = new Set();
+    filesChanged.forEach(file => {
+        const dirname = path.dirname(file);
+        if (dirname.includes("charts")) {
+            const chartDir = dirname.split("/").slice(0, 2).join("/");
+            dirs.add(chartDir);
+        }
+    });
+    console.log("Dirs:", dirs);
+    return Array.from(dirs).sort();
+}
+
+// Function to count the number of charts changed
+function countChartsChanged(chartsDirsChanged) {
+    return chartsDirsChanged.filter(chartDir => chartDir.includes("charts")).length;
+}
+
+// Function to count the number of version bumps
+function countVersionBumps(filesChangedData) {
+    return filesChangedData.reduce((count, file) => {
+        if (file.filename.endsWith("Chart.yaml") && file.patch.includes("+version")) {
+            count += 1;
+        }
+        return count;
+    }, 0);
+}
+
+async function main() {
+    console.log("Checking if charts were changed and version bumps were found...");
+
+    const GITHUB_REPOSITORY = core.getInput('repoName');
+    const PULL_REQUEST_NUMBER = core.getInput('prNumber');
+
+    const filesChangedData = await getFilesChanged(GITHUB_REPOSITORY, PULL_REQUEST_NUMBER);
+    const filesChanged = getFilenames(filesChangedData);
+    const chartsDirsChanged = getChartsDirsChanged(filesChanged);
+    const numChartsChanged = countChartsChanged(chartsDirsChanged);
+    const numVersionBumps = countVersionBumps(filesChangedData);
+
+    console.log("Files Changed:", filesChanged);
+    console.log("Charts Dirs Changed:", chartsDirsChanged);
+    console.log("Num Charts Changed:", numChartsChanged);
+    console.log("Num Version Bumps:", numVersionBumps);
+
+    if (numChartsChanged === 0 && numVersionBumps === 0) {
+        console.log("No charts were changed and no version bumps were found. Exiting...");
+        process.exit(0);
+    }
+
+    if (numChartsChanged !== numVersionBumps) {
+        console.log("Error: Number of charts changed does not match the number of version bumps.");
+        process.exit(1);
+    }
+
+    if (numChartsChanged === numVersionBumps) {
+        console.log("Charts were changed and version bumps were found. All good!");
+        process.exit(0);
+    }
+}
+
+main().catch(error => {
+    console.error(error);
+    process.exit(1);
+});
